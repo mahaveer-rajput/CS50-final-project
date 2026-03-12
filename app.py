56d9441d5182892660057714695a5772bcf14af4
mahaveer_rajput <mahaveerrai57@gmail.com>
2026-03-12 11:13:41 -0700

    UI is Ready

--- app.py
+++ app.py
@@ -3,8 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
 
+    images = [
+        "do1.jpg",
+        "do2.jpg",
+        "do3.jpg",
+        "do4.jpg",
+        "do5.jpg",
+        "do6.jpg",
+        "do7.jpg",
+        "do8.jpg",
+        "do9.jpg",
+        "do10.jpg",
+    ]
+
+    return render_template("index.html", images=images)
 if __name__ == "__main__":
     app.run(debug=True)
