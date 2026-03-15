cece66ca65f0fb928fa713477e1f259a9a60e30b
mahaveer_rajput <mahaveerrai57@gmail.com>
2026-03-15 15:24:59 -0700

    added upload functionality

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,17 @@
-window.addEventListener("scroll", function (){
-    const navbar = document.querySelector(".navbar");
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
 
-    if (window.scrollY > 50){
-      navbar.classList.add("scrolled");
-    } else{
-      navbar.classList.remove("scrolled");
-    }
-})
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+    navbar.style.color = 'Black';
+  } else {
+    navbar.classList.remove("scrolled");
+    navbar.style.color = "white";
 
+  }
+});
 
 function toggleMenu() {
   document.getElementById("navLinks").classList.toggle("active");
 }
+
